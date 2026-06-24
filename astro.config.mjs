91b8956f6c089721aabd7e9b662884994cc67449
Hamadah <muhammadrozaan@gmail.com>
2026-06-24 12:52:38 +0700

    feat(adapter): use cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,16 @@ import react from '@astrojs/react'
 import markdoc from '@astrojs/markdoc'
 import keystatic from '@keystatic/astro'
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  output: 'static',
 
-    integrations: [react(), markdoc(), keystatic()],
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [react(), markdoc(), keystatic()],
+  adapter: cloudflare(),
 })
